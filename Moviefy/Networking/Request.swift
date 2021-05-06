0c55476a3560ba16c20dbff4fbc2b31cbab6f4f3
Hydrochlorick <hydrochlorick@icloud.com>
2021-05-06 03:36:16 -0700

    Configure URL Request and create Encoder to do so

--- Moviefy/Networking/Request.swift
+++ Moviefy/Networking/Request.swift
@@ -26,4 +26,26 @@ struct Request {
         "Content-Type": "application/json",
         "Bearer": "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNTY3NjRhZjU1NjE2MTdmOWQ4NTBhYTIyMWVlNjQxOSIsInN1YiI6IjYwOTFiYWNjM2ZlNzk3MDA1NzE1NDRlMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.svoucgvBx93wBAFMi7xuolHmr0NQTSsAjH8x_TcnEJg"
     ]
+    
+    static func configureRequest(from route: Route, with parameters: [String: Any], and method: HTTPMethod, contains body: Data?) throws -> URLRequest {
+        // Safely create valid URL or return error
+        guard let url = URL(string: "https://api.themoviedb.org/3/\(route.rawValue)") else { fatalError("Error while unwrapping URL") }
+        var request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10.0)
+        request.httpMethod = method.rawValue
+        request.httpBody = body
+        try configureParametersAndHeaders(parameters: parameters, headers: headers, request: &request)
+        return request
+    }
+    
+    static func configureParametersAndHeaders(parameters: [String: Any]?, headers: [String: String]?, request: inout URLRequest) throws {
+        do {
+            if let headers = headers, let parameters = parameters {
+                try Encoder.encodeParameters(for: &request, with: parameters)
+                try Encoder.setHeaders(for: &request, with: headers)
+            }
+        } catch {
+                throw NetworkError.encodingFailed
+        }
+    }
 }
+
